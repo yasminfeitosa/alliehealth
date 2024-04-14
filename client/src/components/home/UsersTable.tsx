47c07549fcf99dff5a6a5192996fddc5c93781ac
yasminfeitosa <yasmin.feitosa@outsmartdigital.com.br>
2024-04-14 15:40:51 -0400

    [FEAT] Configuring router

--- client/src/components/home/UsersTable.tsx
+++ client/src/components/home/UsersTable.tsx
@@ -6,6 +6,9 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import dayjs from 'dayjs';
+import { Link } from "react-router-dom";
+import { User } from "../../types/User";
+import { useUserContext } from "../../contexts/userContext";
 
 const TableHeaderCell = (props: Record<any, any>) => (
   <TableCell
@@ -17,41 +20,46 @@ const TableHeaderCell = (props: Record<any, any>) => (
 );
 
 type Props = {
-  users: {
-    id: number;
-    first_name: string;
-    last_name: string;
-    email: string;
-    birthdate: string;
-  }[];
+  users: User[];
 };
 
-const UsersTable = ({ users }: Props) => (
-  <TableContainer component={Paper}>
-    <Table aria-label="simple table">
-      <TableHead>
-        <TableRow>
-          <TableHeaderCell>Name</TableHeaderCell>
-          <TableHeaderCell>Email</TableHeaderCell>
-          <TableHeaderCell align="right">Birthdate</TableHeaderCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {users.map((user) => (
-          <TableRow
-            key={user.id}
-            sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-          >
-            <TableCell component="th" scope="row">
-              {`${user.first_name} ${user.last_name}`}
-            </TableCell>
-            <TableCell>{user.email}</TableCell>
-            <TableCell align="right">{user.birthdate ? dayjs(user.birthdate).format('MM/DD/YYYY') : 'Not informed'}</TableCell>
+
+const UsersTable = ({ users }: Props) => {
+  const { setUser } = useUserContext();
+
+  const handleLinkOnClick = (user: User) => {
+    setUser(user);
+  }
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableHeaderCell>Name</TableHeaderCell>
+            <TableHeaderCell>Email</TableHeaderCell>
+            <TableHeaderCell align="right">Birthdate</TableHeaderCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </TableContainer>
-);
+        </TableHead>
+        <TableBody>
+          {users.map((user) => (
+            <TableRow
+              key={user.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                <Link to={'/details'} onClick={() => handleLinkOnClick(user)}>
+                  {`${user.first_name} ${user.last_name}`}
+                </Link>
+              </TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell align="right">{user.birthdate ? dayjs(user.birthdate).format('MM/DD/YYYY') : 'Not informed'}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
 
 export default UsersTable;
